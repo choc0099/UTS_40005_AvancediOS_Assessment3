7c84075d85614380be855c6b823f789177a34e57
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-09-18 21:09:47 +1000

    Added API Key
    Decided that I wanted to make an app that will browse accomodation for tourist using the Hotels.com API.

--- Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/ViewModels/MainViewModel.swift
+++ Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/ViewModels/MainViewModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+class HotelBrowserMainViewModel: ObservableObject, Codable {
+    
+    //these are the headers to initialise the API request
+    let headers = [
+        "API_KEY" : "fdc2564bbemshd3b062f571b3b8cp173b6ejsn78fc48e2f6b0",
+        "API_HOST" : "hotels4.p.rapidapi.com"
+    ]
+}

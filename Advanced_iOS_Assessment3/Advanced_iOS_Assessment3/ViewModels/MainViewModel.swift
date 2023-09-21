4d77e91bc9b82b9ac5efa54e497271e12fa5d429
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-09-21 14:24:19 +1000

    api works but unable to Parse JSON

--- Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/ViewModels/MainViewModel.swift
+++ Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/ViewModels/MainViewModel.swift
@@ -12,31 +12,31 @@ class HotelBrowserMainViewModel: ObservableObject {
     
     //these are the headers to initialise the API request
     let headers = [
-        "X-RapidAPI-Key" : "fdc2564bbemshd3b062f571b3b8cp173b6ejsn78fc48e2f6b0",
-        "X-RapidAPI-Host" : "hotels4.p.rapidapi.com"
+        "X-RapidAPI-Key": "fdc2564bbemshd3b062f571b3b8cp173b6ejsn78fc48e2f6b0",
+        "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
     ]
-    
-    var token: String? //used for auth requests
-    var transactionId: Int = 1
+
+
     
     //this is a function the will return an URL Request
     func hotelApi(_ path: String) -> URLRequest {
         
-        let apiUrl = "http://hotels4.p.rapidapi.com"
+        let apiUrl = "https://hotels4.p.rapidapi.com"
         
-        var url = URL(string: apiUrl + path)
+        var url = URL(string: apiUrl + path)! as URL
         
-        var request = URLRequest(url: url!)
+        var request = URLRequest(url: url as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
         //puts the header into the url request
         request.allHTTPHeaderFields = headers
         return request
     }
     
+    
     func loadRegions() async {
         //gets the request with the location search
         //i have hard coded the query strings for now.
-        var request = hotelApi("/locations/v3/search?q=sydney&locale-en_AU")
-        
+        var request = hotelApi("/locations/v3/search?sydney&locale-en_AU")
+    //https://docs-assets.developer.apple.com/published/bbc1147cb0/22fc548e-7697-4dae-a05a-71803b10082e.png
         do {
             //sends the url request
             let (data, _) = try await URLSession.shared.data(for: request)
@@ -48,11 +48,17 @@ class HotelBrowserMainViewModel: ObservableObject {
                 //adds it to the view model structs so it can be displayed in the UI
                 self.hotelSearchResults = response.searchResults
             }
-            
         }
         catch {
             print("Something went wrong \(error)")
         }
         
+    }
+    
+    func initialiseSession(request: URLRequest) -> URLSessionDataTask {
+        let session = URLSession.shared
+        
+        let datatask = session.dataTask(with: request)
+        return datatask
     }
 }

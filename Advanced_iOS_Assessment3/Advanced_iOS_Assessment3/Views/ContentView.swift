3a7aa0313008cf14cc25d15675e0bed2758509b5
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-09-26 16:41:08 +1000

    Moved the search stuff into a new view file called SearchView

--- Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/Views/ContentView.swift
+++ Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/Views/ContentView.swift
@@ -17,91 +17,21 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-    @State var searchText: String = ""
-    @State var errorText: String = "" //displays a message to the user.
-
     var body: some View {
         
         TabView {
-            NavigationStack {
-                //this is for the search fields to search for a query including hotels and regions
-                HStack(spacing: 5) {
-                    Image(systemName: "magnifyingglass")
-                    TextField("Search", text: $searchText).onSubmit {
-                        //sets the search status to loading mode
-                        hotelMain.searchStatus = .loading
-                        Task {
-                            //loads the list of hotels using the api
-                           await hotelMain.loadRegions(query: searchText)
-                            //clears the search field after submission.
-                            searchText = ""
-                        }
-                      
-                    }
-                }.padding().background(Color(.systemGray6))
-                    .cornerRadius(10)
-                    .padding()
-                Group { //the group property is used to add more views based on different conditions.
-                        
-                    if hotelMain.searchStatus == .active {
-                        List {
-                            Section("Hotel Suggestions") {
-                                ForEach(hotelMain.hotelSearchResults) { hotel in
-                                    if hotel.type == "HOTEL" {
-                                        if let haveAddress = hotel.hotelAddress {
-                                            Text(hotel.regionNames.fullName)
-                                        }
-                                    }
-                                }
-                            }
-                            Section("Regions and Neighbourhoods") {
-                                ForEach(hotelMain.regionSearchResults) {
-                                    region in
-                                    if region.type != "HOTEL" {
-                                        Text(region.regionNames.primaryDisplayName)
-                                    }
-                                }
-                                
-                            }
-                        }
-                    }
-                    else {
-                        VStack(spacing: 10) {
-                            if hotelMain.searchStatus == .loading {
-                                ProgressView()
-                            }
-                            else {
-                                //displays messages to the user including error messages.
-                                Text("\(errorText)")
-                            }
-                        }.frame(maxHeight: .infinity)
-                    }
-                }.onAppear {
-                    //adds text depnding on scenarios
-                    switch(hotelMain.searchStatus) {
-                    case .welcome:
-                        errorText = "Welcome to Hotel Browser"
-                    case .noResults:
-                        errorText = "No Results Found"
-                    case .offline:
-                        errorText = "You are currently offline."
-                    case .unkown:
-                        errorText = "Something went wrong!"
-                    default:
-                        errorText = ""
-                    }
-                }
-            }.tabItem {
-                Label("Home", systemImage: "house.fill")
+            SearchView().tabItem {
+                Label("Search", systemImage: "magnifyingglass")
             }
             
             RegionsView().tabItem {
-                Label("Regions", systemImage: "globe")
+                Label("Settings", systemImage: "globe")
             }
-        }.padding()
-        
+        }
     }
-
+        
+    
+    /*
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
@@ -116,6 +46,7 @@ struct ContentView: View {
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
+        
     }
 
     private func deleteItems(offsets: IndexSet) {
@@ -131,8 +62,9 @@ struct ContentView: View {
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
-    }
+    }*/
 }
+
 
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()

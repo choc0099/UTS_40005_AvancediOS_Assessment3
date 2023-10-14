b4076f43a0da09f00996bca426af451fe0168c98
Christopher Av <choclate00@live.com>
2023-10-15 08:59:05 +1100

    UserDefaults searchPrefernces are now allocated to specific users which means each user has their own preferences.

--- Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/Managers/UserDefaultsManager.swift
+++ Advanced_iOS_Assessment3/Advanced_iOS_Assessment3/Managers/UserDefaultsManager.swift
@@ -35,15 +35,31 @@ class UserDefaultsManager {
     
     //saves the property search stuff to user defaults
     public static func savePropertySearchPrefernces(propertySearchPreferences propertyRequest: PropertyListRequest) {
+        //this is used to allocate the specific settings to each user accounts stored on the device
+        var IN_SCOPE_PROPERTY_SEARCH_KEY: String
+        if let haveUser = FirebaseAuthManager.authRef.currentUser {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = "\(PROPERTY_SEARCH_KEY).\(haveUser.uid)"
+        } else {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = PROPERTY_SEARCH_KEY
+        }
+        
         //encodes it to JSON
         let encodedPropertySearch = try! JSONEncoder().encode(propertyRequest)
         //saves it to user defaults
-        standard.setValue(encodedPropertySearch, forKey: PROPERTY_SEARCH_KEY)
+        standard.setValue(encodedPropertySearch, forKey: IN_SCOPE_PROPERTY_SEARCH_KEY)
     }
     
     //loads it from user defaults
     public static func loadPropertySearchData() -> PropertyListRequest? {
-        if let haveData = standard.data(forKey: PROPERTY_SEARCH_KEY) {
+        //this is used to allocate the specific settings to each user accounts stored on the device
+        var IN_SCOPE_PROPERTY_SEARCH_KEY: String
+        if let haveUser = FirebaseAuthManager.authRef.currentUser {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = "\(PROPERTY_SEARCH_KEY).\(haveUser.uid)"
+        } else {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = PROPERTY_SEARCH_KEY
+        }
+        
+        if let haveData = standard.data(forKey: IN_SCOPE_PROPERTY_SEARCH_KEY) {
             let data = try? JSONDecoder().decode(PropertyListRequest.self, from: haveData)
             return data
         }
@@ -52,6 +68,14 @@ class UserDefaultsManager {
     
     //this is a function to remove the preferences from user default and resets it to the default one.
     static func removePreferences() {
-        standard.removeObject(forKey: PROPERTY_SEARCH_KEY)
+        //this is used to allocate the specific settings to each user accounts stored on the device
+        var IN_SCOPE_PROPERTY_SEARCH_KEY: String
+        if let haveUser = FirebaseAuthManager.authRef.currentUser {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = "\(PROPERTY_SEARCH_KEY).\(haveUser.uid)"
+        } else {
+            IN_SCOPE_PROPERTY_SEARCH_KEY = PROPERTY_SEARCH_KEY
+        }
+        //deletes the prefences from user defaults.
+        standard.removeObject(forKey: IN_SCOPE_PROPERTY_SEARCH_KEY)
     }
 }
